28601a9457e3369407b10f201ccbf53481e279c6
yasinarafatajad <yasinarafatajad082@gmail.com>
2024-12-18 00:58:35 +0600

    update Banner.jsx

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -65,15 +65,15 @@ const Banner = () => {
             </div>
           </Link>
           <div className="menubar flex flex-row gap-2">
-            <a src={'/profile'} className="btn btn-circle swap swap-rotate text-[#323232] dark:text-[#fbfcfc] text-center ">
+            <Link to={'/profile'} className="btn btn-circle swap swap-rotate text-[#323232] dark:text-[#fbfcfc] text-center ">
               <IoPerson size={25} />
             </a>
             <a href="mailto:yasinarafatazad173@gmail.com" target="_blank" className="btn btn-circle swap swap-rotate text-[#323232] dark:text-[#fbfcfc] text-center ">
               <TbMessageDots size={25} />
             </a>
-            <a src={'/'} className="btn btn-circle swap swap-rotate text-[#323232] dark:text-[#fbfcfc] text-center ">
+            <Link to={'/'} className="btn btn-circle swap swap-rotate text-[#323232] dark:text-[#fbfcfc] text-center ">
               <AiFillHome size={25} />
-            </a>
+            </Link>
           </div>
         </div>
       </section>
