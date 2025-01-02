988870fc48ca2b23ce40ffb1a7d483a9e5e503a9
yasinarafatajad <yasinarafatazad082@gmail.com>
2025-01-02 21:08:48 +0600

    upload beta postImg

--- src/components/Post/PostCard.tsx
+++ src/components/Post/PostCard.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import PostHeader from './PostHeader';
 import PostImage from './PostImage';
