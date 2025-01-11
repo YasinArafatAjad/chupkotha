b77ff4cc91cdb9a4f1fde975842260c59b19d191
Yasin Arafat Ajad <136703649+YasinArafatAjad@users.noreply.github.com>
2025-01-11 16:00:25 +0600

    img fix

--- src/lib/firebase/posts/validators.ts
+++ src/lib/firebase/posts/validators.ts
@@ -7,9 +7,9 @@ export const validatePostInput = (image: File | null, caption: string) => {
 };
 
 export const validateImageFile = (file: File) => {
-  if (!file.type.startsWith('image/')) {
-    throw new Error('Please select an image file');
-  }
+  //if (!file.type.startsWith('image/')) {
+  //  throw new Error('Please select an image file');
+  //}
 
   // if (file.size > 5 * 1024 * 1024) {
   //   throw new Error('Image size should be less than 5MB');
--- src/lib/services/chat/imageUploadService.ts
+++ src/lib/services/chat/imageUploadService.ts
@@ -3,9 +3,9 @@ import toast from 'react-hot-toast';
 
 export async function uploadChatImage(file: File): Promise<string> {
   try {
-    if (!file.type.startsWith('image/')) {
-      throw new Error('Please select an image file');
-    }
+  //  if (!file.type.startsWith('image/')) {
+  //    throw new Error('Please select an image file');
+  //  }
 
     // if (file.size > 5 * 1024 * 1024) {
     //   throw new Error('Image must be less than 5MB');
